a47942dd24e915a10dea29e745eb89a234b07f6e
Skip <skip@usecurling.com>
2026-03-18 13:09:57 +0000

    feat: Implement recurring billing and dashboard

--- supabase/functions/functions/index.ts
+++ supabase/functions/functions/index.ts
@@ -2,9 +2,7 @@ import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
 
 Deno.serve(async (req: Request) => {
   return new Response(
-    JSON.stringify({ message: 'Dummy function to fix entrypoint missing error' }),
-    {
-      headers: { 'Content-Type': 'application/json', Connection: 'keep-alive' },
-    },
+    JSON.stringify({ message: 'Placeholder function to resolve missing entrypoint error' }),
+    { headers: { 'Content-Type': 'application/json' } },
   )
 })
